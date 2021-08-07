fd0a413c8289245ea4ee3c2ec450011dce764d5a
Kaustubh_rai14 <rkush214@gmail.com>
2021-08-07 14:13:59 +0530

    Join and create

---
+++ public/js/dasboard.js
@@ -0,0 +1,13 @@
+var modal = document.getElementById('modal-wrapper-join');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal = document.getElementById('modal-wrapper-create');
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
